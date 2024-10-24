0dad8187d9b80ef31447c4663b9884da0b2863f3
Volodymyr Karpenko <vol.karpenko07@gmail.com>
2024-10-24 13:45:18 +0200

    HW8: add user and ticket DAO, connection database class, and fixes model classes

--- src/main/java/com/jfb/tickets/model/UserRole.java
+++ src/main/java/com/jfb/tickets/model/UserRole.java
@@ -2,8 +2,15 @@ package com.jfb.tickets.model;
 
 import com.jfb.tickets.enums.Role;
 
+import java.time.Instant;
+import java.util.UUID;
+
 public abstract class UserRole extends BaseModel {
     Role role;
+
+    public UserRole(UUID id, Instant creationTime) {
+        super(id, creationTime);
+    }
 
     public Role getRole() {
         return this.role;
